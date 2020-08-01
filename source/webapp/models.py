318b7f820f4aa1db703f1cf21a9ac347e9399b4d
Beknasar <680633@gmail.com>
2020-08-01 16:05:26 +0600

    Изменён тип данных для дат в models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -11,8 +11,8 @@ class GuestBook(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя автора записи')
     email = models.EmailField(verbose_name='Почта')
     text = models.TextField(max_length=2000, verbose_name='Текст записи')
-    date_create = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    date_update = models.DateField(auto_now=True, verbose_name='Дата обновления')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     status = models.CharField(max_length=25, default=DEFAULT_STATUS, choices=STATUS_CHOICES, verbose_name='Статус')
     def __str__(self):
         return f'{self.name} - {self.email}'
